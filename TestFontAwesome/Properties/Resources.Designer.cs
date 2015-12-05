b64ffdd0d4355a24cabc84e9a90d6a2b4a122529
mkoertgen <marcel.koertgen@gmail.com>
2015-12-05 17:36:18 +0100

    resharpened

--- TestFontAwesome/Properties/Resources.Designer.cs
+++ TestFontAwesome/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace TestFontAwesome.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestFontAwesome.Properties.Resources", typeof(Resources).Assembly);
+                    var temp = new global::System.Resources.ResourceManager("TestFontAwesome.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
