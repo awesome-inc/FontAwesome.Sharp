b72ab3ef0df7e0facc5ba90b07ac5bb25ad02a07
Marcel Körtgen <marcel.koertgen@gmail.com>
2017-09-17 18:54:48 +0200

    cleanup amendment

--- FontAwesome.Sharp/IconFlip.cs
+++ FontAwesome.Sharp/IconFlip.cs
@@ -1,30 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FontAwesome.Sharp
 {
     /// <summary>
-    /// Flip flags
+    ///     Flip flags
     /// </summary>
     [Flags]
     public enum IconFlip : byte
     {
         /// <summary>
-        /// Flip off
+        ///     Flip off
         /// </summary>
         None = 0x0,
+
         /// <summary>
-        /// Horizontal flip
+        ///     Horizontal flip
         /// </summary>
         Horizontal = 0x1,
+
         /// <summary>
-        /// Vertical flip
+        ///     Vertical flip
         /// </summary>
         Vertical = 0x2,
+
         /// <summary>
-        /// Full flip - same as Rotate(180)
+        ///     Full flip - same as Rotate(180)
         /// </summary>
         Full = 0x4
     }
