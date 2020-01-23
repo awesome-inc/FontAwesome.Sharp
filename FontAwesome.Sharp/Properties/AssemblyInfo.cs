8288407536cdb03e129032da44d39e8ae13aab59
mkoertgen <marcel.koertgen@gmail.com>
2020-01-23 16:19:05 +0100

    (netcore3) Added test projects back to solution

---
+++ FontAwesome.Sharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsPrefix("http://schemas.awesome.incremented/wpf/xaml/fontawesome.sharp", "fa")]
+[assembly: XmlnsDefinition("http://schemas.awesome.incremented/wpf/xaml/fontawesome.sharp", "FontAwesome.Sharp")]
+[assembly: InternalsVisibleTo("FontAwesome.Sharp.Tests")]
