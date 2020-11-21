a7b7edc6c7556d128c98f00d7b9f7121104953e7
Marcel Körtgen <marcel.koertgen@gmail.com>
2020-11-21 14:40:30 +0100

    Add public signing, fixes #56

--- FontAwesome.Sharp.Pro/Properties/AssemblyInfo.cs
+++ FontAwesome.Sharp.Pro/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Windows.Markup;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsPrefix("http://schemas.awesome.incremented/wpf/xaml/fontawesome.sharp.pro", "faPro")]
 [assembly: XmlnsDefinition("http://schemas.awesome.incremented/wpf/xaml/fontawesome.sharp.pro", "FontAwesome.Sharp.Pro")]
-[assembly: InternalsVisibleTo("FontAwesome.Sharp.Tests")]
+[assembly: InternalsVisibleTo("FontAwesome.Sharp.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009d3c7c4ace17c970a11cd07316f19e5a7d0a982813b1195cd07422c54390d8db3722832fdb3e196a9e569b66d2d2839de470579114dbab6c66ed8e50f018b8029eb9c3698cbd29d6d4f737994e110df76a7c779c40598ca9f626b1df7046fce4d2e48827cd993032105dcf7e9bbc4054d51bf84c9a02f72df76f26f5c7ff0dc5")]
