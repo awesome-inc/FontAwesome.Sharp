fdb9f9b845e0df4d29d75ab00aa48a3341afe073
Marcel Körtgen <marcel.koertgen@gmail.com>
2022-03-13 12:28:39 +0100

    Updates (net6)

--- build/Build.cs
+++ build/Build.cs
@@ -5,7 +5,6 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.ReportGenerator;
@@ -17,58 +16,13 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
 // NUKE CI Integration: http://www.nuke.build/docs/authoring-builds/ci-integration.html
-[GitHubActions("build", GitHubActionsImage.WindowsLatest,
+[GitHubActions("build", GitHubActionsImage.WindowsServer2022,
     AutoGenerate = false,
     InvokedTargets = new[] {nameof(CiBuild)})]
 // ReSharper disable once CheckNamespace
+// ReSharper disable once ClassNeverInstantiated.Global
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main() => Execute<Build>(x => x.Test);
-
-    #region Parameters
-    const string Framework = "netcoreapp3.1";
-
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
-
-    [Solution] readonly Solution Solution;
-    [GitVersion(Framework = Framework, NoFetch = true)] readonly GitVersion GitVersion;
-
-    [Parameter("The SonarQube login token")]
-    readonly string SonarLogin = Environment.GetEnvironmentVariable("SONAR_LOGIN");
-
-    [Parameter("The SonarQube server")]
-    readonly string SonarServer = IsLocalBuild ? "http://localhost:9000" : "https://sonarcloud.io";
-
-    [Parameter("The SonarQube organization")]
-    readonly string SonarOrganization = "awesome-inc";
-
-
-    [Parameter("Enable coverlet diagnostics (log.*.txt)")]
-    readonly bool CoverletDiag;
-
-    [Parameter("Is CI Build")]
-    readonly bool IsCiBuild = Host is GitHubActions;
-    [Parameter("Push built NuGet package")]
-    readonly bool IsPushTag = (Environment.GetEnvironmentVariable("GITHUB_REF") ?? "-unset-").StartsWith("refs/tags/");
-
-    [Parameter("NuGet API Key")]
-    readonly string NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY");
-    [Parameter("NuGet Source")]
-    readonly string NuGetSource = "https://www.nuget.org";
-
-    static readonly AbsolutePath TestsDirectory = RootDirectory / "tests";
-    static readonly AbsolutePath ArtifactsDir = RootDirectory / ".artifacts";
-
-    #endregion
-
-
     //-------------------------------------------------------------
     // ReSharper disable once UnusedMember.Local
     Target Clean => _ => _
@@ -109,14 +63,8 @@ class Build : NukeBuild
             DotNetTest(settings => settings
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
-                .EnableNoBuild()
-                .EnableBlameMode()
-                .AddLoggers("trx")
-                .EnableCollectCoverage()
+                .SetFramework("net6.0-windows") // Framework
                 .SetDataCollector("XPlat Code Coverage")
-                .SetSettingsFile("coverage.runsettings")
-                .SetProperty("CopyLocalLockFileAssemblies", true)
-                .SetDiagnosticsFile(CoverletDiag ? "log.txt" : null)
             );
         });
 
@@ -125,53 +73,56 @@ class Build : NukeBuild
         .Executes(() =>
         {
             ReportGeneratorTasks.ReportGenerator(settings => settings
-                .SetFramework("net5.0")
-                .SetReports("**/coverage.opencover.xml")
+                .SetFramework(Framework)
+                .SetReports("**/coverage.cobertura.xml")
                 .SetReportTypes(ReportTypes.Cobertura, ReportTypes.SonarQube, ReportTypes.Html)
                 .SetTargetDirectory(".coverage/")
             );
-
         });
 
     //-------------------------------------------------------------
     Target Sonar => _ => _
-            .Description("SonarQube analysis")
-            .DependsOn(SonarBegin)
-            .DependsOn(Coverage)
-            .DependsOn(SonarEnd)
-            .Executes(() =>
-            {
-            });
+        .Description("SonarQube analysis")
+        .DependsOn(SonarBegin)
+        .DependsOn(Coverage)
+        .DependsOn(SonarEnd)
+        .Executes(() =>
+        {
+        });
 
     Target SonarBegin => _ => _
         .Before(Test)
         .Executes(() =>
         {
             SonarScannerTasks.SonarScannerBegin(settings => settings
-                    .SetProjectKey("awesome-inc_FontAwesome.Sharp")
-                    .SetName("FontAwesome.Sharp")
-                    .SetVersion(GitVersion.FullSemVer)
-                    .SetVSTestReports("**/*.trx")
-                    // cf.: https://github.com/nuke-build/nuke/issues/377#issuecomment-595276623
-                    .SetFramework("netcoreapp3.0")
-                    .SetLogin(SonarLogin) // TODO: should be secret -> SetArgument
-                    .SetServer(SonarServer)
-                    .SetProcessArgumentConfigurator(args =>
+                .SetProjectKey("awesome-inc_FontAwesome.Sharp")
+                .SetName("FontAwesome.Sharp")
+                .SetVersion(GitVersion.FullSemVer)
+                .SetVSTestReports("**/*.trx")
+                // cf.: https://github.com/nuke-build/nuke/issues/377#issuecomment-595276623
+                .SetFramework("net5.0") //Framework)
+                .SetLogin(SonarLogin) // TODO: should be secret -> SetArgument
+                .SetServer(SonarServer)
+                .SetProcessArgumentConfigurator(args =>
+                {
+                    // monorepo hack: tell Sonar to scan sub-project only
+                    args.Add($"/d:sonar.projectBaseDir={RootDirectory}");
+                    // generic coverage data, cf.: https://docs.sonarqube.org/latest/analysis/generic-test/
+                    args.Add("/d:sonar.coverageReportPaths=.coverage/SonarQube.xml");
+                    if (!string.IsNullOrWhiteSpace(SonarLogin))
                     {
-                        // monorepo hack: tell Sonar to scan sub-project only
-                        args.Add($"/d:sonar.projectBaseDir={RootDirectory}");
-                        // generic coverage data, cf.: https://docs.sonarqube.org/latest/analysis/generic-test/
-                        args.Add($"/d:sonar.coverageReportPaths=.coverage/SonarQube.xml");
-                        if (!string.IsNullOrWhiteSpace(SonarLogin))
+                        // set organization & branch name, cf.:
+                        // - https://github.com/nuke-build/nuke/issues/304#issuecomment-522250591
+                        // - http://www.nuke.build/docs/authoring-builds/cli-tools.html#custom-arguments
+                        args.Add($"/o:{SonarOrganization}");
+                        if (GitVersion.BranchName != "main")
                         {
-                            // set organization & branch name, cf.:
-                            // - https://github.com/nuke-build/nuke/issues/304#issuecomment-522250591
-                            // - http://www.nuke.build/docs/authoring-builds/cli-tools.html#custom-arguments
-                            args.Add($"/o:{SonarOrganization}")
-                                .Add($"/d:sonar.branch.name={GitVersion.BranchName}");
+                            args.Add($"/d:sonar.branch.name={GitVersion.BranchName}");
                         }
-                        return args;
-                    })
+                    }
+
+                    return args;
+                })
             );
         });
 
@@ -180,9 +131,9 @@ class Build : NukeBuild
         .Executes(() =>
         {
             SonarScannerTasks.SonarScannerEnd(settings => settings
-                .SetLogin(SonarLogin) // TODO: should be secret -> SetArgument
-                                      // cf.: https://github.com/nuke-build/nuke/issues/377#issuecomment-595276623
-                .SetFramework("netcoreapp3.0")
+                    .SetLogin(SonarLogin) // TODO: should be secret -> SetArgument
+                    // cf.: https://github.com/nuke-build/nuke/issues/377#issuecomment-595276623
+                    .SetFramework("net5.0") // Framework
             );
         });
 
@@ -207,27 +158,56 @@ class Build : NukeBuild
         .DependsOn(Package)
         .Executes(() =>
         {
-            if (IsCiBuild && !IsPushTag)
-            {
-                Logger.Info("Skipping (no tag)");
-                return;
-            }
-
-
             DotNetNuGetPush(settings => settings
                 .SetTargetPath(ArtifactsDir / "*.nupkg")
                 .SetApiKey(NuGetApiKey)
                 .SetSource(NuGetSource)
-                );
+            );
         });
 
     //-------------------------------------------------------------
     // ReSharper disable once UnusedMember.Local
     Target CiBuild => _ => _
         .Description("DevOps build target")
-        .DependsOn(Test)
-        .DependsOn(Push)
+        .DependsOn(IsPushTag ? new[] {Sonar, Push} : new[] {Test})
         .Executes(() =>
         {
         });
+
+    /// Support plugins are available for:
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Test);
+
+    #region Parameters
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
+
+    [Solution] readonly Solution Solution;
+
+    const string Framework = "net6.0";
+    [GitVersion(Framework = Framework, NoFetch = true)] readonly GitVersion GitVersion;
+
+    [Parameter("The SonarQube login token")]
+    readonly string SonarLogin = Environment.GetEnvironmentVariable("SONAR_LOGIN");
+
+    [Parameter("The SonarQube server")]
+    readonly string SonarServer = IsLocalBuild ? "http://localhost:9000" : "https://sonarcloud.io";
+
+    [Parameter("The SonarQube organization")] readonly string SonarOrganization = "awesome-inc";
+
+    [Parameter("Push built NuGet package")]
+    readonly bool IsPushTag = (Environment.GetEnvironmentVariable("GITHUB_REF") ?? "-unset-").StartsWith("refs/tags/");
+
+    [Parameter("NuGet API Key")] readonly string NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY");
+
+    [Parameter("NuGet Source")] readonly string NuGetSource = "https://www.nuget.org";
+
+    static readonly AbsolutePath TestsDirectory = RootDirectory / "tests";
+    static readonly AbsolutePath ArtifactsDir = RootDirectory / ".artifacts";
+
+    #endregion
 }
