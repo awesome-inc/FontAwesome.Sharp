21ea93986a94095db6bb0e3d61cee9ce0cb3987d
Marcel Körtgen <marcel.koertgen@gmail.com>
2022-06-26 12:21:24 +0200

    fa6 update (content)

--- Update-Fonts.ps1
+++ Update-Fonts.ps1
@@ -16,7 +16,7 @@ function Update-Files {
 
 Write-Host Updating node packages...
 npm i
-ncu -u
+npx ncu -u
 npm i
 
 Write-Host Updating web fonts...
@@ -26,7 +26,7 @@ Update-Files .\node_modules\@fortawesome\*.ttf .\FontAwesome.Sharp\fonts
 Update-Files .\node_modules\@mdi\*.ttf .\FontAwesome.Sharp.Material\fonts
 
 Write-Host Updating css...
-Update-Files .\node_modules\@fortawesome\fontawesome-free\css\fontawesome.css  .\FontEnumGenerator\Content
+Update-Files .\node_modules\@fortawesome\fontawesome-free\css\all.css  .\FontEnumGenerator\Content
 Update-Files .\node_modules\@mdi\font\css\materialdesignicons.css .\FontEnumGenerator\Content
 
 Write-Host Rebuild .\FontEnumGenerator
@@ -35,7 +35,7 @@ dotnet build .\FontEnumGenerator -c Release
 Write-Host Generating IconEnum classes...
 Push-Location .\FontEnumGenerator\bin\Release
 
-.\FontEnumGenerator.exe --css .\Content\fontawesome.css --name IconChar
+.\FontEnumGenerator.exe --css .\Content\all.css --name IconChar
 Copy-Item -Path .\IconChar.cs -Destination ..\..\..\FontAwesome.Sharp
 
 .\FontEnumGenerator.exe --css .\Content\materialdesignicons.css --pattern "\.mdi-(.+):before" --name MaterialIcons
