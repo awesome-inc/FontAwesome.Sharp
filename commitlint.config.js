7a11ad13a5f3e3f0d9404e92084027ef8d69b9b7
Marcel Körtgen <marcel.koertgen@gmail.com>
2023-03-04 15:47:52 +0100

    style: add conventional commits & spell checking

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+// https://commitlint.js.org/#/reference-configuration?id=javascript
+const Configuration = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // https://commitlint.js.org/#/reference-rules
+    "scope-enum": [2, "always", []],
+  },
+};
+
+module.exports = Configuration;
